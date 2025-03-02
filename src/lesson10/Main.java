f53a6897125ed88aee3a13b1205a32ad8f6fa008
BrunoFerrary <0974582854m@gmail.com>
2025-03-02 11:51:55 +0100

    Додано виведення всіх чисел масиву

--- src/lesson10/Main.java
+++ src/lesson10/Main.java
@@ -2,6 +2,13 @@ package lesson10;
 
 public class Main {
     public static void main(String[] args) {
+
         int[] numbers = {11, 22, 33, 44, 55, 69, 77}; // Масив чисел
+
+        System.out.println("Всі числа масиву:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
     }
 }
