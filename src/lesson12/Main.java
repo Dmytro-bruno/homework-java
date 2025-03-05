a0653b48388cea56ca310f0a9e1274aecb988f48
BrunoFerrary <0974582854m@gmail.com>
2025-03-05 15:19:31 +0100

    Додано зчитування розмірів масиву

--- src/lesson12/Main.java
+++ src/lesson12/Main.java
@@ -1,4 +1,15 @@
 package lesson12;
 
+import java.util.Scanner; // Імпортуємо клас Scanner для введення даних
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Це створюємо об'єкт Scanner
+
+        // Тут зчитуємо розміри масиву від користувача
+        System.out.print("Введіть кількість рядків (n): ");
+        int n = scanner.nextInt();
+        System.out.print("Введіть кількість стовпців (m): ");
+        int m = scanner.nextInt();
+    }
 }
