0d8660e3c0b3deebc0f43971642eb30cd5526ae5
BrunoFerrary <0974582854m@gmail.com>
2025-03-10 23:04:05 +0100

    додано Enum DragonType для класифікації драконів

---
+++ src/lesson14/DragonType.java
@@ -0,0 +1,9 @@
+package lesson14;
+
+public enum DragonType {
+    FIRE,      // Вогняний дракон
+    ICE,       // Крижаний дракон
+    LIGHTNING, // Дракон блискавки
+    EARTH,     // Земляний дракон
+    SHADOW     // Тіньовий дракон
+}
