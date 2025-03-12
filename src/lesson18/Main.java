a46c5bd2d565190b35550df4317e0595736957c1
BrunoFerrary <0974582854m@gmail.com>
2025-03-12 23:00:16 +0100

    Моя машина на ходу!

---
+++ src/lesson18/Main.java
@@ -0,0 +1,22 @@
+package lesson18;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        // Створюємо об'єкт автомобіля
+        Automobile car = new Automobile(50.0, 7.5, LocalDate.of(2025, 12, 1));
+
+        // Виводимо інформацію про автомобіль
+        System.out.println("Інформація про авто: " + car);
+
+        // Перевіряємо, чи може автомобіль їхати
+        System.out.println("Машина може їхати: " + car.canDrive());
+
+        // Перевіряємо, чи дійсний техогляд
+        System.out.println("Машина допущена до експлуатації: " + car.isInspectionValid());
+
+        // Обчислюємо максимальну відстань, яку можна проїхати
+        System.out.println("Максимальна відстань: " + car.calculateMaxDistance() + " км");
+    }
+}
