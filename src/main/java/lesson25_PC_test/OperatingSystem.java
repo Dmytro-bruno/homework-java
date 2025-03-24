e35412c5c01ffe002f223f3ac5f779d8ea5b450c
BrunoFerrary <0974582854m@gmail.com>
2025-03-24 16:30:46 +0100

    🖥️ Додано класи Computer, OperatingSystem та тести

---
+++ src/main/java/lesson25_PC_test/OperatingSystem.java
@@ -0,0 +1,11 @@
+package lesson25_PC_test;
+
+/**
+ * Enum для операційної системи компʼютера.
+ */
+public enum OperatingSystem {
+    WINDOWS, // Windows OS
+    MACOS,   // MacOS
+    LINUX,   // Linux
+    UBUNTU   // Ubuntu
+}
